e23452755e1b698c30cc798a83cb7b3e0d01b5fc
g_larose <g.larose@frontier.com>
2018-09-23 02:49:30 -0400

    added PlayerRecViewAdapter class - a recyclerview adapter class to load players

--- app/src/main/java/com/inkedapparelonline/weeklyfootballpicks/activity/MainActivity.java
+++ app/src/main/java/com/inkedapparelonline/weeklyfootballpicks/activity/MainActivity.java
@@ -7,21 +7,37 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.inkedapparelonline.weeklyfootballpicks.R;
+import com.inkedapparelonline.weeklyfootballpicks.adapters.PlayerRecViewAdapter;
 import com.inkedapparelonline.weeklyfootballpicks.helpers.MatchUpHelper;
 import com.inkedapparelonline.weeklyfootballpicks.model.MatchUp;
+import com.inkedapparelonline.weeklyfootballpicks.model.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FloatingActionButton fab;
+
+    FirebaseDatabase mData;
+    DatabaseReference mDataRef;
+    RecyclerView mainRecView;
+    PlayerRecViewAdapter playerAdapter;
+    List<Player> playerList;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -30,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-
+                    new Load_Players().execute();
                     return true;
                 case R.id.navigation_matchups:
 
@@ -46,7 +62,8 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton.OnClickListener mOnClickListener = (new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, Create_User.class));
+          startActivity(new Intent(MainActivity.this, Create_User.class));
+
         }
     });
 
@@ -55,10 +72,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+        mData = FirebaseDatabase.getInstance();
+        mDataRef = mData.getReference("players");
+
+        playerList = new ArrayList<>();
+        mainRecView = findViewById(R.id.mainActivity_recView);
+
+
+
         fab = findViewById(R.id.main_activity_fab);
         fab.setOnClickListener(mOnClickListener);
-        mAuth = FirebaseAuth.getInstance();
+
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    public  class Load_Players extends AsyncTask<Void, Void, Void> {
+
+        List<Player> list = new ArrayList<>();
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            mDataRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot players : dataSnapshot.getChildren()) {
+                        String playerName = players.child("name").getValue().toString();
+                        String company = players.child("company").getValue().toString();
+                        String id = players.getKey();
+                        int wins = Integer.parseInt(players.child("winTotal").getValue().toString());
+                        int losses = Integer.parseInt(players.child("lossTotal").getValue().toString());
+                        list.add(new Player(playerName, company, id, wins, losses));
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mainRecView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+            playerAdapter = new PlayerRecViewAdapter(MainActivity.this, list);
+            mainRecView.setAdapter(playerAdapter);
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
     }
 }
---
+++ app/src/main/java/com/inkedapparelonline/weeklyfootballpicks/adapters/PlayerRecViewAdapter.java
@@ -0,0 +1,61 @@
+package com.inkedapparelonline.weeklyfootballpicks.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.inkedapparelonline.weeklyfootballpicks.R;
+import com.inkedapparelonline.weeklyfootballpicks.model.Player;
+
+import java.util.List;
+
+public class PlayerRecViewAdapter  extends RecyclerView.Adapter<PlayerRecViewAdapter.PlayerViewHolder>{
+
+    Context context;
+    List<Player> playerList;
+
+    public PlayerRecViewAdapter(Context context, List<Player> playerList) {
+        this.context = context;
+        this.playerList = playerList;
+    }
+
+    @NonNull
+    @Override
+    public PlayerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.player_single_row_item, parent, false);
+        return new PlayerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlayerViewHolder holder, int position) {
+
+        holder.name.setText(playerList.get(position).getName());
+        holder.company.setText(playerList.get(position).getCompany());
+        String win = String.valueOf(playerList.get(position).getWinTotal());
+        holder.wins.setText(win);
+        String loss = String.valueOf(playerList.get(position).getLossTotal());
+        holder.losses.setText(loss);
+    }
+
+    @Override
+    public int getItemCount() {
+        return playerList.size();
+    }
+
+    public class PlayerViewHolder extends RecyclerView.ViewHolder{
+
+        TextView name, company, wins, losses;
+
+        public PlayerViewHolder(View itemView) {
+            super(itemView);
+            name    = itemView.findViewById(R.id.player_card_tv_playername);
+            company = itemView.findViewById(R.id.player_card_tv_companyrname);
+            wins    = itemView.findViewById(R.id.player_single_row_tvWins);
+            losses  = itemView.findViewById(R.id.player_single_row_tvLosses);
+        }
+    }
+}
